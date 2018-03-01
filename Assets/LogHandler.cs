d8f262260cb2d58a49c353c1411b8730d5284ee2
yuuki-ojs <m.yuki.711@gmail.com>
2018-03-02 01:31:11 +0900

    Logger周りの動作確認

---
+++ Assets/LogHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestProject
+{
+    public class LogHandler : ILogHandler
+    {
+
+        const String LF = "¥n";
+
+        [SerializeField]
+        Text logText;
+
+        public LogHandler()
+        {
+            //デバッグの場合のみ
+            if (Debug.isDebugBuild)
+            {
+                // ログが出力されたときのイベントに画面出力を追加
+                Application.logMessageReceived += (string condition, string stackTrace, LogType type) =>
+                {
+                    logText.text += condition + LF;
+                    Debug.Log("stackTrace : " + stackTrace);
+                };
+            }
+        }
+
+        /// <summary>
+        /// Logs the exception.
+        /// </summary>
+        /// <param name="exception">Exception.</param>
+        /// <param name="context">Context.</param>
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
+            Debug.unityLogger.LogException(exception, context);
+        }
+
+        /// <summary>
+        /// Logs the format.
+        /// </summary>
+        /// <param name="logType">Log type.</param>
+        /// <param name="context">Context.</param>
+        /// <param name="format">Format.</param>
+        /// <param name="args">Arguments.</param>
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.unityLogger.logHandler.LogFormat(logType, context, format, args);
+        }
+
+    }
+}
---
+++ Assets/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestProject
+{
+    public class Logger : MonoBehaviour
+    {
+        UnityEngine.Logger logger;
+
+        // Use this for initialization
+        void Start()
+        {
+            logger = new UnityEngine.Logger(new TestProject.LogHandler());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
